fda1b8f7e86dde7a8f4aa9d43b81b14cc75ba73a
Alex <galvan.alex121@gmail.com>
2022-05-05 15:08:10 -0400

    make it better

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,8 +8,8 @@ function About(){
                 <h4 className="about-header">ABOUT ME</h4>
                 <p className="about-msg-content">My name is Alex Galvan. I have been develpoing applications 
                                                 for about over 2 years now. I started out building tools to make
-                                                coworker's life easier. I am starting the Awesome Inc bootcamp this
-                                                fall of 2022 and am eager to get learning new tech! 
+                                                coworker's life easier. I am starting the Awesome Inc bootcamp in
+                                                the fall of 2022 and am eager to get learning new tech! 
                                                 ps: I am the one on the right! The left is my partner 
                                                 in crime & my wife Celine.
                 </p>

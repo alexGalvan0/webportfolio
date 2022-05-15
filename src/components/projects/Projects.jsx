d90642dbcba80526c108e0639a00c19ee3771a91
Alex <galvan.alex121@gmail.com>
2022-05-15 10:16:50 -0400

    make it better

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,6 +13,25 @@ function Projects(){
                     <KANYE_QUOTE />
                     <FlaskWeather/> 
                 </div>
+                <hr/>
+                <div className="project-headers">
+                    <h2 className="api-work">APPLICATIONS:</h2>
+                </div>
+                <div className="applications-container">
+                    <div className="merchtool">
+                        <h3 className="example1">Merch Tool-</h3>
+                        <p>Creatined a quoting application using HTML5 CSS3 JAVASCRIPT 
+                            for the front end, and PYTHON Flask for the backend. 
+                        </p>
+                    </div>   
+                    <hr/>   
+                    <div className="asana">
+                        <h3 className="example2">Asana Automations-</h3>
+                        <p>Creatined a quoting application using HTML5 CSS3 JAVASCRIPT 
+                            for the front end, and PYTHON Flask for the backend. 
+                        </p>
+                    </div>                
+                </div>
         </div>
     )}
 export default Projects;
